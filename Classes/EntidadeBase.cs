b77d88b2423f5e30a2d438fc29765ab78d4d3f05
Lucas <lucasakajiri@hotmail.com>
2021-02-10 11:57:02 -0300

    "parte 2 da aula"

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace Dio_Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id {get; protected set;}
+        
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,40 @@
+using System;
+namespace Dio_Series
+{
+    public class Serie : EntidadeBase
+    {
+        //atributos
+        private Genero Genero {get;set;}
+        private string Titulo{get;set;}
+        private string Descricao{get;set;}
+        private int Ano {get;set;}
+        
+        //metodos
+
+        public Serie(int id, Genero genero, string titulo, string descricao,int ano){
+            this.Id=id;
+            this.Genero=genero;
+            this.Titulo=titulo;
+            this.Descricao=descricao;
+            this.Ano=ano;
+        }//inicializador
+
+        public override string ToString()
+        {
+            string retorno ="";
+            retorno+="Género: " + this.Genero + Environment.NewLine;
+            retorno+="Titulo: " + this.Titulo + Environment.NewLine;
+            retorno+="Descrição: " + this.Descricao + Environment.NewLine;
+            retorno+="Ano de Inicio: " + this.Ano + Environment.NewLine;
+            return retorno;
+        }//toString
+
+        public string retornaTitulo(){
+            return this.Titulo;
+        }
+        public int retornaID(){
+            return this.Id;
+        }
+
+    }
+}
